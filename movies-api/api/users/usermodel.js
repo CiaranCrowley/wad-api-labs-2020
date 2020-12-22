fdee8221dfeb8e8a7840cd0b0fe5d84ac00e829c
Ciaran Crowley <ciarancrowley37@gmail.com>
2020-12-22 00:47:20 +0000

    Week 9 refactor Movie api

--- movies-api/api/users/usermodel.js
+++ movies-api/api/users/usermodel.js
@@ -3,8 +3,10 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const MovieSchema = new Schema({
-  id: Number,
-  title: String
+  // id: Number,
+  // title: String
+  id: { type: Number, unique: true, required: true},
+  title: { type: String, unique: true, required: true}
 });
 
 const UserSchema = new Schema({
