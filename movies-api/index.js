0c7142d5d17dff43eec50cdf54090e5a72a26864
Ciaran Crowley <ciarancrowley37@gmail.com>
2020-12-22 16:53:08 +0000

    Week 9 Included Mongoose

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,17 +2,23 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 import bodyParser from 'body-parser';
+import './db';
+import {loadUsers} from './seedData'
 
 dotenv.config();
 
 const errHandler = (err, req, res, next) => {
-   /* if the error in development then send stack trace to display whole error,
+	/* if the error in development then send stack trace to display whole error,
    if it's in production then just send error message  */
    if(process.env.NODE_ENV === 'production') {
      return res.status(500).send(`Something went wrong!`);
    } else {
       res.status(500).send(`Hey!! You caught the error 👍👍, ${err.stack} `);
    };
+};
+
+if (process.env.SEED_DB) {
+	loadUsers();
 };
 
 const app = express();
@@ -27,5 +33,5 @@ app.use('/api/movies', moviesRouter);
 app.use(errHandler);
 
 app.listen(port, () => {
-  console.info(`Server running at ${port}`);
+	console.info(`Server running at ${port}`);
 });
