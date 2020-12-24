7a4f3c56eed6ad55e07abffb7fc778d82b1f4f76
Ciaran Crowley <ciarancrowley37@gmail.com>
2020-12-24 14:49:32 +0000

    Topic 10 - Session based Auth

--- movies-api/index.js
+++ movies-api/index.js
@@ -3,19 +3,21 @@ import express from 'express';
 import moviesRouter from './api/movies';
 import bodyParser from 'body-parser';
 import './db';
-import {loadUsers} from './seedData';
+import { loadUsers } from './seedData';
 import usersRouter from './api/users';
+import session from 'express-session';
+import authenticate from './authenticate';
 
 dotenv.config();
 
 const errHandler = (err, req, res, next) => {
 	/* if the error in development then send stack trace to display whole error,
-   if it's in production then just send error message  */
-   if(process.env.NODE_ENV === 'production') {
-     return res.status(500).send(`Something went wrong!`);
-   } else {
-      res.status(500).send(`Hey!! You caught the error 👍👍, ${err.stack} `);
-   };
+	if it's in production then just send error message  */
+	if(process.env.NODE_ENV === 'production') {
+		return res.status(500).send(`Something went wrong!`);
+	} else {
+		res.status(500).send(`Hey!! You caught the error 👍👍, ${err.stack} `);
+	};
 };
 
 if (process.env.SEED_DB) {
@@ -30,8 +32,20 @@ app.use(express.static('public'));
 //configure body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded())
-app.use('/api/movies', moviesRouter);
+
+//session middleware
+app.use(session({
+	secret: 'ilikecake',
+	resave: true,
+	saveUninitialized: true
+}));
+
+//update /api/Movie route
+app.use('/api/movies', authenticate, moviesRouter);
+
+// app.use('/api/movies', moviesRouter);
 app.use('/api/genres', moviesRouter);
+
 //Users router
 app.use('/api/users', usersRouter);
 app.use(errHandler);
